267959fb89126bd3222108305ff76b6d2c6efafd
sknapp88 <knapp533@gmail.com>
2017-06-03 10:05:58 -0400

    worked on connection and server

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE `burgers_db`;
 CREATE TABLE 'burgers' (
 	`id` INT AUTO_INCREMENTING,
 	`burger_name` VARCHAR(50) NOT NULL,
-	`devoured` BOOLEAN,
+	`devoured` BOOLEAN default false,
 	`date` TIMESTAMP,
 	PRIMARY KEY (`id`)
 );
--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
+var app = express();
+var exphbs = require("express-handlebars");
+var connection = require("./config/connection.js");
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var port = 3000;
+
+app.get("/", function(req, res){
+	res.render("index", {});
+});
+
+
+
+app.listen(port, function(){
+	console.log("hello from" + port);
+});
